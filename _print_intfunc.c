9be3a756b4e8728422ea61ca2162f343ab47e2d8
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-29 03:00:17 +0000

    updated _print_intfunc.c

--- _print_intfunc.c
+++ _print_intfunc.c
@@ -8,36 +8,36 @@
 
 int _print_intfunc(va_list num)
 {
-    unsigned int tmp;
-    int n = 0, i = 0, x = 0, count = 0;
-    n = va_arg(num, int);
-    if (n <= INT_MAX && n >= INT_MIN)
-    {
-        if (n < 0)
-        {
-            _putchar('-');
-            count++;
-            n = -n;
-        }
-        tmp = n;
-        while (tmp / 10 > 0)
-        {
-            tmp /= 10;
-            i++;
-        }
-        tmp = n;
-        while (i >= 0)
-        {
-            for (x = 0; x < i; x++)
-                tmp /= 10;
-            tmp %= 10;
-            _putchar(tmp + '0');
-            count++;
-            i--;
-            tmp = n;
-        }
-    }
-    else
-        return (-1);
-    return (count);
+	unsigned int tmp;
+	int n = 0, i = 0, x = 0, count = 0;
+	n = va_arg(num, int);
+	if (n <= INT_MAX && n >= INT_MIN)
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			count++;
+			n = -n;
+		}
+		tmp = n;
+		while (tmp / 10 > 0)
+		{
+			tmp /= 10;
+		i++;
+		}
+		tmp = n;
+		while (i >= 0)
+		{
+			for (x = 0; x < i; x++)
+			tmp /= 10;
+			tmp %= 10;
+			_putchar(tmp + '0');
+			count++;
+			i--;
+			tmp = n;
+		}
+	}
+	else
+		return (-1);
+	return (count);
 }
