09c2a47fbf35dadb2a1fba98de51486aa946e69d
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-29 03:24:31 +0000

    Updated _print_binfunc.c

--- _printbinfunc.c
+++ _printbinfunc.c
@@ -1,48 +1,21 @@
 #include "main.h"
 
 /**
- * _print_binfunc - prints an integer as a string.
- * @newargums: positive number to be printed
- * Return: number of characters printed
+ * _print_binfunc - prints a number in binary
+ * @newargums: number to be printed
+ * Return: count of digits
  */
 
 int _print_binfunc(va_list newargums)
 {
 	unsigned int num = va_arg(newargums, int);
-	int i, rder, counter = 0, index = 0, temp_num = num, num_digits = 0;
-	char *str;
+	int count;
 
-	if (num <= UINT_MAX)
-	{
-		do {
-		num_digits++;
-		temp_num /= 10;
-		} while (temp_num > 0);
+	count = 0;
+	if (num / 2 != 0)
+		count += print_binary(num / 2);
 
-		/* allocate a buffer to hold the string representation of the number*/
-		str = malloc(sizeof(char) * num_digits + 1);
-		if (str == NULL)
-			return (-1);
-
-		else
-		{
-			/* convert the number to binary*/
-			do {
-			rder = num % 2;
-			num = num / 2;
-			str[index++] = rder + '0';
-			} while (num > 0);
-			str[index] = '\0';
-			/* write the string to stdout in reverse order*/
-			for (i = index - 1; i >= 0; i--)
-			{
-				counter += _putchar(str[i]);
-			}
-		}
-	}
-	else
-	{
-		return (-1);
-	}
-	return (counter);
+	_putchar (num % 2 == 0 ? '0' : '1');
+	count++;
+	return (count);
 }
