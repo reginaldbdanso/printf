ca14142a6c17594da0af4a8359b7ab5055f0dcbc
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-29 16:35:14 +0000

    updated printbinfunc

--- _printbinfunc.c
+++ _printbinfunc.c
@@ -8,38 +8,42 @@
 
 int _print_binfunc(va_list newargums)
 {
-	unsigned int num;
-	int num1 = va_arg(newargums, int);
-	int i, rder, counter = 0, index = 0, temp_num, num_digits = 0;
+	unsigned int num = va_arg(newargums, int);
+	int i, rder, counter = 0, index = 0, temp_num = num, num_digits = 0;
 	char *str;
 
-	if (num1 < 0)
-		num1 *= -1;
-	num = (unsigned int)num1;
-	temp_num = num;
-	do {
-	num_digits++;
-	temp_num /= 10;
-	} while (temp_num > 0);
-
-	/* allocate a buffer to hold the string representation of the number*/
-	str = malloc(sizeof(char) * num_digits + 1);
-	if (str == NULL)
-		return (-1);
-
-	/* convert the number to binary*/
-	do {
-	rder = num % 2;
-	num = num / 2;
-	str[index++] = rder + '0';
-	} while (num > 0);
-
-	/* write the string to stdout in reverse order*/
-	for (i = index - 1; i >= 0; i--)
+	if (num <= UINT_MAX)
 	{
-		counter += write(1, &str[i], 1);
-	}
+		do {
+		num_digits++;
+		temp_num /= 10;
+		} while (temp_num > 0);
 
+		/* allocate a buffer to hold the string representation of the number*/
+		str = malloc(sizeof(char) * num_digits + 1);
+		if (str == NULL)
+			return (-1);
+
+		else
+		{
+			/* convert the number to binary*/
+			do {
+			rder = num % 2;
+			num = num / 2;
+			str[index++] = rder + '0';
+			} while (num > 0);
+
+			/* write the string to stdout in reverse order*/
+			for (i = index - 1; i >= 0; i--)
+			{
+				counter += write(1, &str[i], 1);
+			}
+		}
+	}
+	else
+	{
+		return (-1);
+	}
 	return (counter);
 }
 
