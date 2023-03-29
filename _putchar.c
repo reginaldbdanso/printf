572db9936cf2b54256544189134733c288e8bff0
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-29 05:10:15 +0000

    updated putchar.c

--- _putchar.c
+++ _putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: the character to be printed
  *
  * Return: On success 1.
  * On error, -1 is returned and errno is set appropriately.
